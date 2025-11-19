72c44b4679b7efb03989ddc79aa5f46551abd115
mahmoud <mahmoodabuhassnah@gmail.com>
2025-11-19 16:43:47 +0300

    Ued flexbox to make the navigation bar more dynamic

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,13 +1,13 @@
 <nav class="navbar">
-    <div class="navdiv">
+   
         <div class="logo"><a href="/final_project/index.php">Ultimate Life Form RetroStore</a></div>
-        <ul>
-            <li><a href="/final_project/index.php">Home</a></li>
-            <li><a href="/final_project/pages/Services.php">Services</a></li>
-            <li><a href="/final_project/pages/Contact.php">Contact</a></li>
-            <li><a href="/final_project/pages/Items.php">Items</a></li>
-            <li><a href="/final_project/pages/form.php">Form</a></li>
-            <li><a href="/final_project/pages/About.php">About</a></li>
-        </ul>
-    </div>
+        
+            <a href="/final_project/index.php">Home</a>
+            <a href="/final_project/pages/Services.php">Services</a>
+            <a href="/final_project/pages/Contact.php">Contact</a>
+            <a href="/final_project/pages/Items.php">Items</a>
+            <a href="/final_project/pages/form.php">Form</a>
+            <a href="/final_project/pages/About.php">About</a>
+        
+    
 </nav>
