4376138749d7aaafd3df58e5324cc7ccfdc5ea83
mahmoud <mahmoodabuhassnah@gmail.com>
2025-12-02 23:09:52 +0300

    Implemented  the session and fetching of data from the database and putting it into the table.

--- pages/table.php
+++ pages/table.php
@@ -1,3 +1,23 @@
+<?php
+session_start();
+require_once '../db_connect.php';
+
+$user_id = $_SESSION["user_id"];
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,39 +37,72 @@
         <?php include '../includes/navbar.php'; ?>
         <h1>Table Page</h1>
         <table class="Table">
-    <caption>Best Selling Games</caption>
+    <caption>Your forms</caption>
 
-    <tr>
-        <th colspan="2">Game</th>
-        <th colspan="2">Genre</th>
-        <th colspan="2">Amount</th>
-    </tr>
+<?php 
+$sql = "SELECT * FROM form WHERE user_id = '$user_id'; ";
 
-    <tr>
-        <th rowspan="2">Best Sell</th>
-        <th>Best Sell</th>
-        <th>Best Sell</th>
-        <th>Best Sell</th>
-        <th>Best Sell</th>
-        <th>Best Sell</th>
-    </tr>
+$result = mysqli_query($conn,$sql);
 
-    <tr>
-        <th>Best Sell</th>
-        <th>Best Sell</th>
-        <th>Best Sell</th>
-        <th>Best Sell</th>
-        <th>Best Sell</th>
-    </tr>
+if($result){
 
-    <tr>
-        <th>Best Sell</th>
-        <th>Best Sell</th>
-        <th>Best Sell</th>
-        <th>Best Sell</th>
-        <th>Best Sell</th>
-        <th>Best Sell</th>
-    </tr>
+if(mysqli_num_rows($result) > 0){
+echo "<tr>";
+echo "<th>user_id</th>";
+echo "<th>sell_or_buy</th>";
+echo "<th>game_name</th>";
+echo "<th>quantity</th>";
+echo "<th>price</th>";
+echo "<th>game_condition</th>";
+echo "<th>feedback</th>";
+
+
+echo "</tr>";
+
+while($row = mysqli_fetch_array($result)){
+echo "<tr>";
+
+echo "<td>" . $row["user_id"] . "</td>";
+echo "<td>" . $row["sell_or_buy"] . "</td>";
+echo "<td>" . $row["game_name"] . "</td>";
+echo "<td>" . $row["quantity"] . "</td>";
+echo "<td>" . $row["price"] . "</td>";
+echo "<td>" . $row["game_condition"] . "</td>";
+echo "<td>" . $row["feedback"] . "</td>";
+
+
+
+echo "</tr>";
+
+
+
+}
+
+
+    
+}
+
+else{
+
+echo "There is no forms that you have submitted";
+
+}
+
+
+
+
+
+
+}
+else{
+    echo "Error: Could not execute your SQL".mysqli_error($conn);
+}
+
+
+?>
+
+
+   
 </table>
         <?php include '../includes/footer.php'; ?>
     </body>
